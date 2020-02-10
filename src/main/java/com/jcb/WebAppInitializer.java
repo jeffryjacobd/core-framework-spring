1ac8781d42b8175cd73c056e26716a019ff3c063
Jeffry Jacob D <jeffryjacobd@gmail.com>
2020-02-10 21:33:59 +0530

    Web Initializer fix

---
+++ src/main/java/com/jcb/WebAppInitializer.java
@@ -0,0 +1,18 @@
+package com.jcb;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+
+import org.springframework.web.WebApplicationInitializer;
+
+public class WebAppInitializer implements WebApplicationInitializer {
+
+	@Override
+	public void onStartup(ServletContext servletContext) throws ServletException {
+		// TODO Auto-generated method stub
+		System.out.println(servletContext);
+	}
+
+
+	}
+

9e31e58a6a3d7fba1ffbef5c632cc8c2f78684d5
Jeffry Jacob D <jeffryjacobd@gmail.com>
2020-02-10 23:00:56 +0530

    pom changes and dispatcher fixed

--- src/main/java/com/jcb/WebAppInitializer.java
+++ src/main/java/com/jcb/WebAppInitializer.java
@@ -2,15 +2,24 @@ package com.jcb;
 
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration;
 
 import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+
+import com.jcb.config.MainConfig;
 
 public class WebAppInitializer implements WebApplicationInitializer {
 
 	@Override
 	public void onStartup(ServletContext servletContext) throws ServletException {
+		AnnotationConfigWebApplicationContext mainContext = new AnnotationConfigWebApplicationContext();
+		mainContext.setConfigLocation(MainConfig.class.getName());
+		ServletRegistration.Dynamic dispatcher = servletContext.addServlet("dispatcher",
+				new DispatcherServlet(mainContext));
+		dispatcher.addMapping("/");
+		dispatcher.setLoadOnStartup(1);
 	}
 
-
-	}
-
+}

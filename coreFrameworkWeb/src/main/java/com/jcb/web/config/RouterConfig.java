4241a5adabab2e79ab325afcbd025f976bd1ec57
Jeffry Jacob D <jeffryjacobd@gmail.com>
2020-06-20 10:35:06 +0530

    Allow all security

--- coreFrameworkWeb/src/main/java/com/jcb/web/config/RouterConfig.java
+++ coreFrameworkWeb/src/main/java/com/jcb/web/config/RouterConfig.java
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
 @Configuration
-@Import({ StaticFilesRouter.class })
+@Import({ StaticFilesRouter.class, WebSecurityConfig.class })
 public class RouterConfig {
 
 }
--- coreFrameworkWeb/src/main/java/module-info.java
+++ coreFrameworkWeb/src/main/java/module-info.java
@@ -4,6 +4,10 @@ module core.framework.web {
 
     requires spring.webflux;
 
+    requires spring.security.config;
+
+    requires spring.security.web;
+
     opens com.jcb.web.config to spring.context, spring.beans, spring.core;
 
     opens com.jcb.web.router to spring.context, spring.beans, spring.core;

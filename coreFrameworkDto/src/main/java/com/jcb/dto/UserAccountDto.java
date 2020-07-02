0b499c0af0ccabef563ef56cffbb3fb1f822cad2
Jeffry Jacob D <jeffryjacobd@gmail.com>
2020-07-02 13:44:08 +0530

    beautify userDto

--- coreFrameworkDto/src/main/java/com/jcb/dto/UserAccountDto.java
+++ coreFrameworkDto/src/main/java/com/jcb/dto/UserAccountDto.java
@@ -3,14 +3,14 @@
  */
 package com.jcb.dto;
 
-import java.time.LocalDate;
-import java.util.List;
-import java.util.UUID;
-
 import com.jcb.annotation.CassandraTable;
 import com.jcb.annotation.ClusteringKeyColumn;
 import com.jcb.annotation.PartitionKeyColumn;
 import com.jcb.constants.enumeration.Gender;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.UUID;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -28,31 +28,31 @@ import lombok.NoArgsConstructor;
 @Builder
 public class UserAccountDto {
 
-	@PartitionKeyColumn(0)
-	private String userName;
+    @PartitionKeyColumn(0)
+    private String userName;
 
-	private UUID companyId;
+    @ClusteringKeyColumn(0)
+    private String firstName;
 
-	@ClusteringKeyColumn(0)
-	private String firstName;
+    @ClusteringKeyColumn(1)
+    private String middleName;
 
-	@ClusteringKeyColumn(1)
-	private String middleName;
+    private String lastName;
 
-	private String lastName;
+    private String password;
 
-	private String password;
+    private UUID companyId;
 
-	private LocalDate dateOfBirth;
+    private LocalDate dateOfBirth;
 
-	private Gender gender;
+    private Gender gender;
 
-	private List<String> roles;
+    private List<String> roles;
 
-	private String email;
+    private String email;
 
-	private String phoneNo;
+    private String phoneNo;
 
-	private String extNo;
+    private String extNo;
 
 }
